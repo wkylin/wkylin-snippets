41fd32150bcd17d7e1d471967bae2b905d64c453
wkylin <wkylin@qq.com>
2014-05-08 13:37:15 +0800

    delete files

--- 垂直二级导航/css/test.css
+++
@@ -1,7 +0,0 @@
-/**
- * Created by JetBrains WebStorm.
- * User: wb-wangjg
- * Date: 12-1-31
- * Time: 下午3:13
- * To change this template use File | Settings | File Templates.
- */
--- 垂直二级导航/js/bb.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by JetBrains WebStorm.
- * User: wb-wangjg
- * Date: 12-2-1
- * Time: ионГ11:15
- * To change this template use File | Settings | File Templates.
- */
